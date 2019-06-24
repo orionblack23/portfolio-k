17f9050645da9cd18699c0759b833ddfd97ce380
ORION BLACK <orionb.y23@gmail.com>
2019-06-24 00:14:07 -0700

    fixing router post bug

--- routes/index.js
+++ routes/index.js
@@ -94,48 +94,7 @@ transporter.sendMail(mailOption, (error, info) => {
   console.log("Message sent: %s", info.messageId);
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 
-  res.render('index',
-    {
-      section1: {
-        sideBarName: 'Home',
-        firstName: 'Lindan',
-        lastName: 'MOYO',
-        imgAvatar: '/images/others/imgAvatar.jpg',
-        profileBackground: '/images/others/profilBackground.jpg',
-        title: 'LINDAN MOYO'
-    },
-  
-    section2: {
-      sideBarName: 'ABOUT',
-      header: 'ABOUT ME',
-      subHeader: 'Welcome on my portfolio',
-      greeting: "I'm lindan MOYO",
-      bio: "Hi! My name is <strong>Lindan MOYO</strong>.I am a Computer Engineer who is self motivated and keen to take up any challenging position to learn new skills to broaden my skills in computing technologies as well as a software developer. l am proficient at working both independently and in a team set up and also thrive in deadline-driven environments.Equipped with strong organizational and analytical skills as well as practical problem-solving , decision making skills andstrong communication skills (written and verbal).",
-      imgAvatar2: '/images/others/imgAvatar2.jpg',
-      birthday: "20.01.1997",
-      age: "22",
-      city: 'Harare',
-      interests: 'Drama',
-      study: 'European University of Lefke',
-      degree: 'Undergraduate',
-      mail: 'lindanmoyo',
-      downloadCv: '/images/others/MOYO CV.pdf'
-    },
-  
-    section3: {
-      header: 'My Abilities',
-      description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since. Lorem Ipsum has been the industry. Lorem Ipsum has been the industry's standard dummy text since. Lorem Ipsum is simply."
-    },
-    section4: {
-      sideBarName: 'CONTACT',
-      header: 'CONTACT ME',
-      subHeader: 'To contact me:',
-      address: '24 Anafat',
-      mail: 'lindanmoyo',
-  
-    },
-  });
-
+  res.redirect('/');
 });
 });
 
